6f755307286765450d113e7cfc2c995955708b31
achim <achim>
2008-10-15 15:13:24 +0000

    work in progress

--- src/config/src/luacfg/cfg.lua
+++ src/config/src/luacfg/cfg.lua
@@ -25,19 +25,20 @@ ConfigMethods = {}
 ConfigMethods.__index = ConfigMethods
  
 
------------------ definitions
+---------------------------------------------------------------------------
+-- BLOCK DEFINITIONS
+---------------------------------------------------------------------------
 
-
-function Config()
+function CONFIG()
    local tab = {}
-   tab.MAT = {} -- subtables
-   tab.SRC = {}
-   tab.DIAG = {}
+   tab.mat = {} -- subtables
+   tab.src = {}
+   tab.diag = {}
    return setmetatable(tab,ConfigMethods)
 end
 
 
--- Grid Block definition
+-- GRID Config-Block definition
 
 function ConfigMethods:GRID(parms)
    self.GRID = { block="GRID" }
@@ -50,14 +51,14 @@ function ConfigMethods:GRID(parms)
    self.GRID.dt = parms.ncyc or 0.9999;
 end
 
--- Fdtd Block definition
+-- FDTD Config-Block definition
 
 function ConfigMethods:FDTD(parms)
    self.FDTD = { block="FDTD" }
    for i,v in ipairs(parms) do self.FDTD[i] = parms[i] end
 end
 
--- Epsilon Sub-Block definition
+-- EPSILON Sub-Block definition
 
 function EPSILON(parms)
    local EPSILON = { block="EPSILON" }
@@ -65,7 +66,7 @@ function EPSILON(parms)
    return EPSILON
 end
 
--- Bound Block definition
+-- BOUND Config-Block definition
 
 function ConfigMethods:BOUND(parms)
    self.BOUND = { block = "BOUND" }
@@ -76,7 +77,7 @@ function ConfigMethods:BOUND(parms)
    for i,v in ipairs(parms) do self.BOUND[i] = parms[i] end
 end
 
--- Pml Sub-Block definition
+-- PML Sub-Block definition
 
 function PML(parms)
    local PML = { block = "PML" }
@@ -88,7 +89,7 @@ function PML(parms)
    return PML
 end
 
--- Reg Sub-Block definition
+-- REG Sub-Block definition
 
 function REG(parms) 
    local REG = { block = "REG" }
@@ -99,62 +100,60 @@ function REG(parms)
    return REG
 end
 
--- Load Sub-Block definition
+-- LOAD Sub-Block definition
 
 function LOAD(parms) 
    local LOAD = { block = "LOAD" }
-   for i,v in ipairs(parms) do LOAD[i] = parms[i] end
+   for k,v in pairs(parms) do LOAD[k] = v end
    return LOAD
 end
 
--- Geo Sub-Block definition
+-- GEO Sub-Block definition
 
 function GEO(parms) 
    local GEO = { block = "GEO" }
-   GEO.scene = parms.scene
-   GEO.grid = parms.grid
-   GEO.method = parms.method
+   for k,v in pairs(parms) do GEO[k] = v end
    return GEO
 end
 
--- Point Sub-Block definition
+-- POINT Sub-Block definition
 
 function POINT(parms) 
    local POINT = { block = "POINT" }
-   for i,v in ipairs(parms) do POINT[i] = parms[i] end
+   for k,v in pairs(parms) do POINT[k] = v end
    return POINT
 end
 
--- Box Sub-Block definition
+-- BOX Sub-Block definition
 
 function BOX(parms) 
    local BOX = { block = "BOX" }
-   for i,v in ipairs(parms) do BOX[i] = parms[i] end
+   for k,v in pairs(parms) do BOX[k] = v end
    return BOX
 end
 
--- Out Sub-Block definition
+-- OUT Sub-Block definition
 
 function OUT(parms) 
    local OUT = { block = "OUT" }
+   for k,v in pairs(parms) do OUT[k] = v end
    OUT.file = parms.file or { "VTK", "?" }
    OUT.type = parms.type or { "?", "N", ".T." }
    OUT.type[2] = OUT.type[2] or "N"
    OUT.type[3] = OUT.type[3] or ".T."
    OUT.time = parms.time or { 0,-1, 1 }  
-   for i,v in ipairs(parms) do OUT[i] = parms[i] end
    return OUT
 end
 
--- Mat Block definition
+-- MAT Config-Block definition
 
 function ConfigMethods:MAT(parms)
    local MAT = { block = "MAT" }
-   for i,v in ipairs(parms) do MAT[i] = parms[i] end
-   table.insert(self.MAT, MAT)   
+   for k,v in pairs(parms) do MAT[k] = v end
+   table.insert(self.mat, MAT)   
 end
 
--- Drude Sub-Block definition
+--(MAT)DRUDE Sub-Block definition
 
 function DRUDE(parms) 
    local DRUDE = { block = "DRUDE" }
@@ -164,20 +163,81 @@ function DRUDE(parms)
    return DRUDE
 end
 
+-- (MAT)LHM Sub-Block definition
 
--- Src Block definition
+function LHM(parms) 
+   local LHM = { block = "LHM" }
+   LHM.invlambdapl = parms.invlambdapl
+   LHM.gammapl = parms.gammapl or 0
+   LHM.order = parms.order or 1
+   return LHM
+end
+
+-- (MAT)PEC Sub-Block definition
+
+function PEC(parms) 
+   local PEC = { block = "PEC" }
+   return PEC
+end
+
+-- (MAT)LORENTZ Sub-Block definition
+
+function LORENTZ(parms) 
+   local LORENTZ = { block = "LORENTZ" }
+   LORENTZ.invlambdal = parms.invlambdal
+   LORENTZ.gammal = parms.gammal or 0
+   LORENTZ.deltaepsl = parms.deltaepsl or 1e-3
+   return LORENTZ
+end
+
+-- (MAT)DEBYE Sub-Block definition
+
+function DEBYE(parms) 
+   local DEBYE = { block = "DEBYE" }
+   DEBYE.taud = parms.taud or 0.
+   DEBYE.deltaepsd = parms.deltaepsd or 1e-3
+   return DEBYE
+end
+
+
+-- (MAT)BLOCH Sub-Block definition
+
+function BLOCH(parms) 
+   local BLOCH = { block = "BLOCH" }
+   BLOCH.invlambdal = parms.invlambdal
+   BLOCH.gammal = parms.gammal or 0
+   BLOCH.dipole = parms.dipole or { {1.,0.},{1.,0.},{1.,0.} }
+   BLOCH.carrier = parms.carrier or { 1., 0.5 }
+   BLOCH.gammanr = parms.gammanr or 0
+   BLOCH.pump = parms.pump or 0
+   BLOCH.satmodel = parms.satmodel or 0
+   return BLOCH
+end
+
+
+-- SRC Config-Block definition
 
 function ConfigMethods:SRC(parms)
    local SRC = { block = "SRC" }
-   for i,v in ipairs(parms) do SRC[i] = parms[i] end
-   table.insert(self.SRC, SRC)   
+   for k,v in pairs(parms) do SRC[k] = v end
+   table.insert(self.src, SRC)   
+end
+
+-- DIAG Config-Block definition
+
+function ConfigMethods:DIAG(parms)
+   local DIAG = { block = "DIAG" }
+   for k,v in pairs(parms) do DIAG[k] = v end
+   table.insert(self.diag, DIAG)   
 end
 
 
 
+---------------------------------------------------------------------------
+-- OUTPUT METHODS
+---------------------------------------------------------------------------
 
------------------ end of definitions; processing follows
-
+-- GRID Config-Block write
 
 local function writeGRID(fh,GRID)
    assert(GRID and GRID.block == "GRID", "Expected Grid{}")
@@ -191,6 +251,8 @@ local function writeGRID(fh,GRID)
    fh:write("  ",GRID.krange[1], " ", GRID.krange[2]," \t! krange = (kbeg,kend)\n");
    fh:write(")GRID\n\n");
 end
+
+-- BOX Sub-Block write
 
 local function writeBOX(fh,BOX)
    assert(BOX and BOX.block == "BOX", "Expected Box{}")
@@ -214,6 +276,8 @@ local function writeBOX(fh,BOX)
    fh:write("      )BOX\n")
 end
 
+-- POINT Sub-Block write
+
 local function writePOINT(fh,POINT)
    assert(POINT and POINT.block == "POINT", "Expected Point{}")
    if POINT.on == false then return end  
@@ -236,6 +300,8 @@ local function writePOINT(fh,POINT)
    fh:write("      )POINT\n")
 end
 
+-- LOAD Sub-Block write
+
 local function writeLOAD(fh,LOAD)
    assert(LOAD and LOAD.block == "LOAD", "Expected Load{}")
    if LOAD.on == false then return end  
@@ -243,6 +309,8 @@ local function writeLOAD(fh,LOAD)
    fh:write("\t",LOAD.file,"\t! file to load\n")
    fh:write("      )LOAD\n")
 end
+
+-- REG Sub-Block write
 
 local function writeREG(fh,REG)
    assert(REG and REG.block == "REG", "Expected Reg{}")
@@ -260,6 +328,8 @@ local function writeREG(fh,REG)
    fh:write("    )REG\n")
 end
 
+-- EPSILON Sub-Block write
+
 local function writeEPSILON(fh,EPSILON)
    assert(EPSILON and EPSILON.block == "EPSILON", "Expected Epsilon{}")
    if EPSILON.on == false then return end  
@@ -268,6 +338,8 @@ local function writeEPSILON(fh,EPSILON)
    writeREG(fh, EPSILON[1])
    fh:write("  )EPSILON\n")
 end
+
+-- OUT Sub-Block write
 
 local function writeOUT(fh,OUT)
    assert(OUT and OUT.block == "OUT", "Expected Out{}")
@@ -281,6 +353,7 @@ local function writeOUT(fh,OUT)
    fh:write("  )OUT\n")
 end
 
+-- FDTD Config-Block write
 
 local function writeFDTD(fh,FDTD)
    assert(FDTD and FDTD.block == "FDTD", "Expected Fdtd{}")
@@ -298,6 +371,8 @@ local function writeFDTD(fh,FDTD)
    fh:write(")FDTD\n\n");
 end
 
+-- PML Sub-Block write
+
 local function writePML(fh,PML)
    assert(PML and PML.block == "PML", "Expected Pml{}")
    fh:write("  (PML\n")
@@ -307,6 +382,8 @@ local function writePML(fh,PML)
    fh:write("    ",PML.kappa,"\t! kappa parameter\n");
    fh:write("  )PML\n")
 end
+
+-- BOUND Config-Block write
 
 local function writeBOUND(fh,BOUND)
    assert(BOUND and BOUND.block == "BOUND", "Expected Bound{}")
@@ -320,6 +397,8 @@ local function writeBOUND(fh,BOUND)
    fh:write(")BOUND\n\n")
 end
 
+-- SRC Config-Block write
+
 local function writeSRC(fh,SRC)
    assert(SRC and SRC.block == "SRC", "Expected Src{}")
    if SRC.on == false then return end  
@@ -330,13 +409,47 @@ local function writeSRC(fh,SRC)
    fh:write(")SRC"..type.."\n\n")
 end
 
+-- (VARIOUS)MAT Sub-Block write
+
 local writemat = {
    DRUDE = function(fh,DRUDE)
 	      fh:write("  ", DRUDE.invlambdapl,"\t! invlambdapl [2 pi c]\n")
 	      fh:write("  ", DRUDE.gammapl,"\t! gammapl (damping) [1/dt]\n")
 	      fh:write("  ", DRUDE.order,"\t! order: 1 (J ode) or 2 (P ode)\n")
+	   end,
+   LHM = function(fh,LHM)
+	      fh:write("  ", LHM.invlambdapl,"\t! invlambdapl [2 pi c]\n")
+	      fh:write("  ", LHM.gammapl,"\t! gammapl (damping) [1/dt]\n")
+	      fh:write("  ", LHM.order,"\t! order: 1 (J ode) or 2 (P ode)\n")
+	   end,
+   LORENTZ = function(fh,LORENTZ)
+	      fh:write("  ", LORENTZ.invlambdal,"\t! invlambdal [2 pi c]\n")
+	      fh:write("  ", LORENTZ.gammal,"\t! gammal (damping) [1/dt]\n")
+	      fh:write("  ", LORENTZ.deltaepsl,"\t! deltaepsl coupling coeff.\n")
+	   end,
+   DEBYE = function(fh,DEBYE)
+	      fh:write("  ", DEBYE.taud,"\t! taud [dt]\n")
+	      fh:write("  ", DEBYE.deltaepsd,"\t! deltaepsd coupling coeff.\n")
+	   end,
+   PEC = function(fh,PEC)
+	 end,
+   BLOCH = function(fh,BLOCH)
+	      fh:write("  ", BLOCH.invlambdal,"\t! invlambdal [2 pi c]\n")
+	      fh:write("  ", BLOCH.gammal,"\t! gammal (damping) [1/dt]\n")
+	      fh:write("  ", 
+		       "(",BLOCH.dipole[1][1],",",BLOCH.dipole[1][2],") ",
+		       "(",BLOCH.dipole[2][1],",",BLOCH.dipole[2][2],") ",
+		       "(",BLOCH.dipole[3][1],",",BLOCH.dipole[3][2],") ",
+		       "\t! dipole matrix elem. []\n")
+	      fh:write("  ", BLOCH.carrier[1]," ", BLOCH.carrier[2],
+		       "\t! carrier numbers transp./initial  []\n")
+	      fh:write("  ", BLOCH.gammanr,"\t! non-rad. recomb. [1/dt]\n")
+	      fh:write("  ", BLOCH.pump,"\t! pump rate [1/dt]\n")
+	      fh:write("  ", BLOCH.satmodel,"\t! sat.model 0=>(N-Ntr), 1=>Ntr*log(N/Ntr)\n")		       
 	   end
 }
+
+-- MAT Config-Block write
 
 local function writeMAT(fh,MAT)
    assert(MAT and MAT.block == "MAT", "Expected Mat{}")
@@ -357,6 +470,8 @@ local function writeMAT(fh,MAT)
    fh:write(")MAT"..type.."\n\n");
 end
 
+-- DIAG  Config-Block write
+
 local function writeDIAG(fh,DIAG)
    assert(DIAG and DIAG.block == "DIAG", "Expected Diag{}")
    if DIAG.on == false then return end  
@@ -367,28 +482,28 @@ local function writeDIAG(fh,DIAG)
    fh:write(")DIAG"..type.."\n\n");
 end
 
-
+-- CREATE configuration file
 
 function ConfigMethods:CREATE()
    local part = self.GRID.partition[1];
-   local fh = io.open("config."..tostring(part)..".in","w");
-   fh:write("\n! ------ begin: file generated by <luacfg>\n\n");
+   local filename = "config."..tostring(part)..".in"
+   local fh = io.open(filename,"w");
+   fh:write("\n! ------ BEGIN [",filename,"] generated by <luacfg>\n\n");
    writeGRID(fh,self.GRID);
    writeFDTD(fh,self.FDTD);
    writeBOUND(fh,self.BOUND);
-   for _, src in ipairs(self.SRC) do writeSRC(fh,src) end
-   for _, mat in ipairs(self.MAT) do writeMAT(fh,mat) end
-   for _, diag in ipairs(self.DIAG) do writeDIAG(fh,diag) end
-   fh:write("! ------ end\n\n");
+   for _, src in ipairs(self.src) do writeSRC(fh,src) end
+   for _, mat in ipairs(self.mat) do writeMAT(fh,mat) end
+   for _, diag in ipairs(self.diag) do writeDIAG(fh,diag) end
+   fh:write("! ------ END [",filename,"] \n\n");
    fh:close();
 end
 
 
+---------------------------------------------------------------------------
+-- FINISH
+---------------------------------------------------------------------------
 
-
-
-
-
------------------ move things into global namespace
+-- move things to global namespace
 
 for k,v in pairs(_M) do _G[k] = v end
