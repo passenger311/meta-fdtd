22488b84d4b29204d1ba64a97dbafbb465cc88d6
achim <achim>
2008-11-04 17:26:08 +0000

    changed mmi_1.lua.

--- src/samples/mmi_1.lua
+++ src/samples/mmi_1.lua
@@ -13,45 +13,65 @@ real_height_wg = 0.220
 real_length_wg1 = 1.4
 real_hwidth_sep = 0.65
 
-
 nref_si = 3.47
 nref_sio2 = 1.444
 
 eps_si = nref_si^2
 eps_sio2 = nref_sio2^2
 
---- device in natural HL, dx=dy=dz=1.
-
-width = 281   -- i
-height = 101  -- j
-length = 800  -- k
-
-maxi = math.floor( (width - 1) / 2  )
-maxj = math.floor( height - 1 )
-maxk = math.floor( length - 1 )
+--- scaling parameters
 
 resolution = 40
 
 real_dx = real_wavelength / nref_si / resolution
 
-grid_height_wg = math.floor(real_height_wg/real_dx+0.5)
-grid_height_bsio2 = math.floor(real_height_bsio2/real_dx)
-grid_hwidth_wg = math.floor(real_width_wg/2/real_dx+0.5)
-grid_hwidth_mmi = math.floor(real_width_mmi/2/real_dx+0.5)
-grid_length_wg1 = math.floor(real_length_wg1/real_dx+0.5)
-grid_length_mmi = math.floor(real_length_mmi/real_dx+0.5)
-grid_hwidth_sep = math.floor(real_hwidth_sep/real_dx+0.5)
+print("resolution = ", resolution)
+print("real_dx = ", real_dx)
+
+
+print("wavelength (grid) = ", real_wavelength/real_dx)
+
+invwavelength = real_dx/real_wavelength;
+
+print("invwavelength (grid) = ", invwavelength)
+
+
+--- device in natural HL, dx=dy=dz=1.
+
+ncyc = 8000
+pulsehwhm = 500
+pulsehsteps = 1000
+
+width = 281   -- i
+height = 101  -- j
+length = 850  -- k
+
+maxi = math.floor( (width - 1) / 2  )
+maxj = math.floor( height - 1 )
+maxk = math.floor( length - 1 )
+
+
+height_wg = math.floor(real_height_wg/real_dx+0.5)
+height_bsio2 = math.floor(real_height_bsio2/real_dx)
+hwidth_wg = math.floor(real_width_wg/2/real_dx+0.5)
+hwidth_mmi = math.floor(real_width_mmi/2/real_dx+0.5)
+length_wg1 = math.floor(real_length_wg1/real_dx+0.5)
+length_mmi = math.floor(real_length_mmi/real_dx+0.5)
+hwidth_sep = math.floor(real_hwidth_sep/real_dx+0.5)
+length_wg2 = maxk - length_wg1 - length_mmi
+
 
 print("irange (grid) = ", -maxi, maxi)
 print("jrange (grid) = ", 0, maxj)
 print("krange (grid) = ", 0, maxk)
-print("height_wg (grid) = ", grid_height_wg)
-print("height_bsio2 (grid) = ", grid_height_bsio2)
-print("hwidth_wg (grid) = ", grid_hwidth_wg)
-print("length_wg1 (grid) = ", grid_length_wg1)
-print("length_mmi (grid) = ", grid_length_mmi)
-print("hwidth_mmi (grid) = ", grid_hwidth_mmi)
-print("hwidth_sep (grid) = ", grid_hwidth_sep)
+print("height_wg (grid) = ", height_wg)
+print("height_bsio2 (grid) = ", height_bsio2)
+print("hwidth_wg (grid) = ", hwidth_wg)
+print("length_wg1 (grid) = ", length_wg1)
+print("length_mmi (grid) = ", length_mmi)
+print("hwidth_mmi (grid) = ", hwidth_mmi)
+print("hwidth_sep (grid) = ", hwidth_sep)
+print("length_wg2 (grid) = ", length_wg2)
 
 print("irange (real) = ", -maxi*real_dx, maxi*real_dx)
 print("jrange (real) = ", 0, maxj*real_dx)
@@ -63,33 +83,33 @@ print("krange (real) = ", 0, maxk*real_dx)
 scene1 = Scene{value=1.}
 box_bsio2 = Box{ 
    from={-maxi-1,-1,-1},
-   to={maxi+1,grid_height_bsio2,maxk+1}
+   to={maxi+1,height_bsio2,maxk+1}
 }
 
 box_wg1 = Box{
-   from={-grid_hwidth_wg,grid_height_bsio2,-1},
-   to={grid_hwidth_wg,grid_height_bsio2+grid_height_wg,grid_length_wg1 }
+   from={-hwidth_wg,height_bsio2,-1},
+   to={hwidth_wg,height_bsio2+height_wg,length_wg1 }
 }
 
 box_mmi = Box{
-   from={-grid_hwidth_mmi,grid_height_bsio2,grid_length_wg1},
-   to={grid_hwidth_mmi,grid_height_bsio2+grid_height_wg,grid_length_wg1+grid_length_mmi }
+   from={-hwidth_mmi,height_bsio2,length_wg1},
+   to={hwidth_mmi,height_bsio2+height_wg,length_wg1+length_mmi }
 }
 
-i0 = -grid_hwidth_sep-grid_hwidth_wg
-i1 = -grid_hwidth_sep+grid_hwidth_wg
+i0 = -hwidth_sep-hwidth_wg
+i1 = -hwidth_sep+hwidth_wg
 
 box_wg2 = Box{
-   from={i0,grid_height_bsio2,grid_length_wg1+grid_length_mmi},
-   to={i1,grid_height_bsio2+grid_height_wg,maxk+1 }
+   from={i0,height_bsio2,length_wg1+length_mmi},
+   to={i1,height_bsio2+height_wg,maxk+1 }
 }
 
-i0 = grid_hwidth_sep-grid_hwidth_wg
-i1 = grid_hwidth_sep+grid_hwidth_wg
+i0 = hwidth_sep-hwidth_wg
+i1 = hwidth_sep+hwidth_wg
 
 box_wg3 = Box{
-   from={i0,grid_height_bsio2,grid_length_wg1+grid_length_mmi},
-   to={i1,grid_height_bsio2+grid_height_wg,maxk+1 }
+   from={i0,height_bsio2,length_wg1+length_mmi},
+   to={i1,height_bsio2+height_wg,maxk+1 }
 }
 
 scene1:add{ box_bsio2, depth=1, value=eps_sio2 }
@@ -97,8 +117,8 @@ scene1:add{ box_wg1, depth=1, value=eps_si }
 scene1:add{ box_mmi, depth=1, value=eps_si }
 scene1:add{ box_wg2, depth=1, value=eps_si }
 scene1:add{ box_wg3, depth=1, value=eps_si }
-grid1 = Grid{from={-maxi,grid_height_bsio2-10,0},to={maxi,grid_height_bsio2+grid_height_wg+1,maxk}}
-gridv =  Grid{yee=false,from={-maxi,grid_height_bsio2-10,0},to={maxi,grid_height_bsio2+grid_height_wg+1,maxk},offset={-maxi,0,0},cells={50,100,100}}
+grid1 = Grid{from={-maxi,height_bsio2-10,0},to={maxi,height_bsio2+height_wg+1,maxk}}
+gridv =  Grid{yee=false,from={-maxi,height_bsio2-10,0},to={maxi,height_bsio2+height_wg+1,maxk},offset={-maxi,0,0},cells={50,100,100}}
 
 cfg:CREATE_GEO{"scene1", scene=scene1, grid=grid1, method="default",comps=3, silent=false, on=false }
 cfg:CREATE_PREVIEW{"scene1", scene=scene1, grid=gridv, method="default", silent=false, on=true }
@@ -110,7 +130,7 @@ cfg:GRID{
 
    dim = 3,
    partition = { 0, 1 },
-   ncyc = 1000,
+   ncyc = ncyc,
    dt = 0.576,
    irange = { -maxi, maxi },
    jrange = { 0, maxj },
@@ -124,8 +144,8 @@ cfg:FDTD{
    EPSILON{
       REG{
 	 BOX{
-	    { -maxi-1, maxi+1, 1, 0, grid_height_bsio2-1, 1, 0, maxk, 1, ":", eps_sio2, eps_sio2, eps_sio2 },
-	    { -maxi-1, maxi+1, 1, grid_height_bsio2, maxj, 1, 0, maxk, 1, ":", eps_si, eps_si, eps_si }
+	    { -maxi-1, maxi+1, 1, 0, height_bsio2-1, 1, 0, maxk, 1, ":", eps_sio2, eps_sio2, eps_sio2 },
+	    { -maxi-1, maxi+1, 1, height_bsio2, maxj, 1, 0, maxk, 1, ":", eps_si, eps_si, eps_si }
 	 },
 	 LOAD_GEO{ "scene1" }
       },
@@ -133,57 +153,91 @@ cfg:FDTD{
    },
 
    OUT{
-      file = { "VTK", "test_xy_en" },
-      type = { "En", "N" },
+      file = { "VTK", "slice1_xy_e" },
+      type = { "E", "N" },
       time = { 0, 1000, 500 },
       REG{
 	 BOX{
-	    { -50,50, 1,-50, 50, 1, 0, 0, 1 }
+	    { -hwidth_wg-10,hwidth_wg+10, 1, 
+	       height_bsio2-10, height_bsio2+height_wg+10, 1, 
+	      math.floor(length_wg1/2), math.floor(length_wg1/2) , 1  }
 	 }
       }
    },
 
    OUT{
-      file = { "VTK", "test_xy_e" },
+      file = { "VTK", "slice2_xy_e" },
       type = { "E", "N" },
       time = { 0, 1000, 500 },
       REG{
 	 BOX{
-	    { -50,50, 1,-50, 50, 1, 0, 0, 1 }
+	    { -hwidth_wg-10,hwidth_wg+10, 1, 
+	       height_bsio2-10, height_bsio2+height_wg+10, 1, 
+	       length_wg1, length_wg1, 1  }
 	 }
       }
    },
 
-   OUT{
-      file = { "VTK", "test_xz_e" },
+    OUT{
+      file = { "VTK", "slice3_xy_e" },
       type = { "E", "N" },
       time = { 0, 1000, 500 },
       REG{
 	 BOX{
-	    { -50, 50, 1, 0, 0, 1, -50, 50, 1 }
+	    { -hwidth_mmi-10,hwidth_mmi+10, 1, 
+	       height_bsio2-10, height_bsio2+height_wg+10, 1, 
+	      length_wg1+math.floor(length_mmi/2),length_wg1+math.floor(length_mmi/2) , 1  }
 	 }
       }
    },
 
-   OUT{
-      file = { "VTK", "test_yz_e" },
-      type = { "E", "N" },
-      time = { 0, 1000, 500 },
-      REG{
-	 BOX{
-	    { 0, 0, 1, -50, 50, 1, -50, 50, 1 }
-	 }
-      }
-   },
+    OUT{
+       file = { "VTK", "slice4_xy_e" },
+       type = { "E", "N" },
+       time = { 0, 1000, 500 },
+       REG{
+	  BOX{
+	     { -hwidth_sep-hwidth_wg-10,hwidth_sep+hwidth_wg+10, 1, 
+	       height_bsio2-10, height_bsio2+height_wg+10, 1, 
+	       length_wg1+length_mmi,length_wg1+length_mmi, 1  }
+	  }
+       }
+    },
+    
+    OUT{
+       file = { "VTK", "slice5_xy_e" },
+       type = { "E", "N" },
+       time = { 0, 1000, 500 },
+       REG{
+	  BOX{
+	     { -hwidth_sep-hwidth_wg-10,hwidth_sep+hwidth_wg+10, 1, 
+	       height_bsio2-10, height_bsio2+height_wg+10, 1, 
+	       length_wg1+length_mmi+math.floor(length_wg2/2),length_wg1+length_mmi+math.floor(length_wg2/2) , 1  }
+	  }
+       }
+    },
+
+      
+    OUT{
+       file = { "VTK", "slice1_xz_e" },
+       type = { "E", "N" },
+       time = { 0, 1000, 500 },
+       REG{
+	  BOX{
+	     { -maxi+11, maxi-11, 1, 
+	       height_bsio2+math.floor(height_wg/2),height_bsio2+math.floor(height_wg/2), 1,
+	       0+11, maxk-11, 1	       
+	    }
+	  }
+       }
+    },
 
 }
 
 --- BOUND Definition
 
 cfg:BOUND{
-
    config = { 1, 1, 1, 1, 1, 1 },
-
    PML{
       cells = 11,
       pot = 3.2,
@@ -195,22 +249,25 @@ cfg:BOUND{
 --- SRC Definition(s)
 
 cfg:SRC{
-   TFSFBOX{
-      invlambda = 0.0333333333,
+   TFSFINJ{
+      invlambda = invwavelength,
       amplitude = 1.,
       pulse = { 
 	 shape="Gaussian", 
-	 width=400,
-	 offset=0, attack=800, sustain=400, decay=800 
+	 width=pulsehwhm,
+	 offset=0, attack=pulsehsteps, sustain=0, decay=pulsehsteps
       },
-      planewave = { phi=0, theta=0.0, psi=90.0, nrefr=1.0 }
+      planewave = { phi=0, theta=0.0, psi=90.0, nrefr=nref_si }
    },
    REG{
       BOX{
-	 { -35, 35, 1, -35, 35, 1, -35, 35, 1 }
+	 { -hwidth_wg+10, hwidth_wg-10, 1, 
+	   height_bsio2+10, height_bsio2+height_wg-10, 1,
+	   30, 30, 1 
+	}
       }
    },
-   on = false
+   on = true
 }
 
 
