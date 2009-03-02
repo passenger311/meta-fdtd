334a14471a9603285c0f34a38f6cb6f03d6d4ba8
sebastian <sebastian>
2009-03-02 23:40:20 +0000

    Near to far field transformation matlab files, ntff.m uses geometric mean for H in normal direction to plane

---
+++ src/other/ntff/readref.m
@@ -0,0 +1,48 @@
+function [E,H] = readface(number,freqnumber)
+
+filename=sprintf('dft%i-ref_%i.set',number,freqnumber);
+fprintf(1,'\nloading file: %s', filename);
+fid = fopen(filename, 'r');
+if (fid==-1) error('file does not exist'); end;
+
+tmp = textscan(fid, '%s',1); clear tmp;
+%tmp = textscan(fid, '%n %n %n %n %n %n %n %n %n', 1,'CollectOutput', 1);
+tmp = textscan(fid, '%n %n %n %n %n %n %n %n %n', 1);
+
+range_tmp = zeros(1,size(tmp,2));
+for j = 1 : size(tmp,2)
+   range_tmp(j) = tmp{j};
+end
+
+facerange(:,:)=reshape(range_tmp,3,3)';
+clear tmp, range_tmp;
+
+if (number == 1)
+   %tmp = textscan(fid,'%n %n %n %n %n %n %n %n %n %n %n %n', 'CollectOutput', 1);
+   %tmp = tmp{1};
+   tmp2 = textscan(fid, '%n %n %n %n %n %n %n %n ', 1);
+
+   tmp = zeros(1,size(tmp2,2));
+   for j = 1 : size(tmp2,2)
+      tmp(j) = tmp2{j};
+   end
+   E = zeros(size(tmp,1),3); H=E;
+   E(:,1:2) = tmp(:,1:2:4)+i*tmp(:,2:2:4);
+   H(:,1:2) = tmp(:,5:2:8)+i*tmp(:,6:2:8);
+
+else
+
+   %tmp = textscan(fid,'%n %n %n %n', 'CollectOutput', 1);
+   %tmp = tmp{1};
+   tmp2 = textscan(fid, '%n %n %n %n', 1);
+
+   tmp = zeros(1,size(tmp2,2));
+   for j = 1 : size(tmp2,2)
+      tmp(j) = tmp2{j};
+   end
+   E = zeros(size(tmp,1),3); H=E;
+   H(:,1:2) = tmp(:,1:2:4)+i*tmp(:,2:2:4);
+
+end
+
+fclose(fid);
