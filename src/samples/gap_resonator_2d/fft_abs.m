0446eb46727f1ca358ed4b160757e4ab0044f358
sebastian <sebastian>
2009-09-09 17:15:41 +0000

    cleaned files

--- src/samples/gap_resonator_2d/fft_abs.m
+++ src/samples/gap_resonator_2d/fft_abs.m
@@ -1,5 +1,4 @@
 s = {'x','y','z'};
-%t = {'cap','capnp','np'};
 
 fid = fopen('data.save','r');
 tline = fgetl(fid); 
@@ -7,79 +6,65 @@ dx = sscanf(tline,'%e');
 tline = fgetl(fid);
 boxsize = sscanf(tline,'%e %e %e');
 fclose(fid);
-%boxsize = {294,98,1};
-%for l = 1:11
-%   fprintf(1,'\nfolder: task%i\n', l);
-%for m = 1:3
-   filename=sprintf('s_abs.pspec');
-   fid3 = fopen(filename,'w');
-   if (fid3==-1) continue; end;
-   fprintf(1,'file: %s\n', filename);
-   for i = 1:3
-      filename=sprintf('fft+%sabs.pspec',s{i});
-      fid = fopen(filename, 'r');
-      filename=sprintf('fft-%sabs.pspec',s{i});
-      fid2 = fopen(filename,'r');
-      if (fid2==-1) continue; end;
-      for j=1:7
-         tline = fgetl(fid);
-         tline = fgetl(fid2);
-         if i==1
-            fprintf(fid3,tline);
-            fprintf(fid3,'\n');
-         end
+filename=sprintf('s_abs.pspec');
+fid3 = fopen(filename,'w');
+if (fid3==-1) continue; end;
+fprintf(1,'file: %s\n', filename);
+for i = 1:3
+  filename=sprintf('fft+%sabs.pspec',s{i});
+  fid = fopen(filename, 'r');
+  filename=sprintf('fft-%sabs.pspec',s{i});
+  fid2 = fopen(filename,'r');
+  if (fid2==-1) continue; end;
+  for j=1:7
+      tline = fgetl(fid);
+      tline = fgetl(fid2);
+      if i==1
+        fprintf(fid3,tline);
+        fprintf(fid3,'\n');
       end
-      tmp = zeros(3,3); wg=zeros(3,3);np=zeros(3,3);
-      k=1;
-      while feof(fid) == 0
-         tline = fgetl(fid);
-         wg(i,:) = sscanf(tline, '%e %e %e');
-         tline = fgetl(fid2);
-         np(i,:) = sscanf(tline, '%e %e %e');
-         tmp(i,1) = 0;
-         lambda(i,k) = np(i,1);
-         for j = 2:1:3; tmp(i,j) = wg(i,j)-np(i,j); end;
-%         srefl(1,1,k)=np(3,2);
-         stot(i,:,k) = tmp(i,:)*(boxsize(mod(i,3)+1))*(boxsize(mod(i+1,3)+1));
-         k=k+1;
-      end;
-      k_max=k;
-      fclose(fid);
-      fclose(fid2);
-   end
-   stotal = sum(sum(stot,1),2);
-   for k=1:k_max-1
-      fprintf(fid3,'%e %e\n', dx/lambda(2,k),dx*stotal(1,1,k));
-   end
-   fclose(fid3);
-   filename=sprintf('../fft_ref-zabs.pspec');
-   fid = fopen(filename, 'r');
-   for j=1:7
+  end
+  tmp = zeros(3,3); wg=zeros(3,3);np=zeros(3,3);
+  k=1;
+  while feof(fid) == 0
       tline = fgetl(fid);
-   end
-   sz=zeros(1,3);
-   k=1;
-   while feof(fid) == 0
-      tline = fgetl(fid);
-      sz(1,:) = sscanf(tline, '%e %e %e');
-      s_inj(1,1,k) = sz(1,2);
+      wg(i,:) = sscanf(tline, '%e %e %e');
+      tline = fgetl(fid2);
+      np(i,:) = sscanf(tline, '%e %e %e');
+      tmp(i,1) = 0;
+      lambda(i,k) = np(i,1);
+      for j = 2:1:3; tmp(i,j) = wg(i,j)-np(i,j); end;
+      stot(i,:,k) = tmp(i,:)*(boxsize(mod(i,3)+1))*(boxsize(mod(i+1,3)+1));
       k=k+1;
-   end
-   fclose(fid);
-   np_abs = stotal ./ s_inj;
-   filename=sprintf('s_relabs.pspec');
-   fid3 = fopen(filename,'w');
-   for k=1:k_max-1
-      fprintf(fid3,'%e %e\n', dx/lambda(2,k),dx*abs(np_abs(k)));
-   end
-   fclose(fid3);
-%  fprintf(1,'file: %s\n', filename);
-%  filename=sprintf('task%i/s_relrefl.pspec',l);
-%  fid4 = fopen(filename,'w');
-%  np_refl = srefl ./ s_inj;
-%  for k=1:k_max-1
-%     fprintf(fid4,'%e %e\n', lambda(1,k),abs(np_refl(k)));
-%  end
-%  fclose(fid4); 
-%end
+  end;
+  k_max=k;
+  fclose(fid);
+  fclose(fid2);
+end
+stotal = sum(sum(stot,1),2);
+for k=1:k_max-1
+  fprintf(fid3,'%e %e\n', dx/lambda(2,k),dx*stotal(1,1,k));
+end
+fclose(fid3);
+filename=sprintf('../fft_ref-zabs.pspec');
+fid = fopen(filename, 'r');
+for j=1:7
+  tline = fgetl(fid);
+end
+sz=zeros(1,3);
+k=1;
+while feof(fid) == 0
+  tline = fgetl(fid);
+  sz(1,:) = sscanf(tline, '%e %e %e');
+  s_inj(1,1,k) = sz(1,2);
+  k=k+1;
+end
+fclose(fid);
+np_abs = stotal ./ s_inj;
+filename=sprintf('s_relabs.pspec');
+fid3 = fopen(filename,'w');
+for k=1:k_max-1
+  fprintf(fid3,'%e %e\n', dx/lambda(2,k),dx*abs(np_abs(k)));
+end
+fclose(fid3);
 exit
