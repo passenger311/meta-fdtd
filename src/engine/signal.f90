511aa76efcc24506248fecb1a53fcdeed963a98d
fr810 <fr810>
2011-09-22 09:52:53 +0000

    Added ramp signal to measure IV curves. Use width parameter as number of steps in attach and decay phase

--- src/engine/signal.f90
+++ src/engine/signal.f90
@@ -62,7 +62,8 @@ real(kind=8) function GenericWave(sigshape ,ncyc, noffs, natt, nsus, ndcy, nhwhm
 
   case( "Sech" )
      GenericWave = SechWave(ncyc, noffs, natt, nsus, ndcy, nhwhm, omega, alpha, domega)
-
+  case ( "Ramp" )
+     GenericWave = RampWave(ncyc, noffs, natt, nsus, ndcy, nhwhm, omega, alpha, domega )
   case default
      GenericWave = GaussianWave(ncyc, noffs, natt, nsus, ndcy, nhwhm, omega, alpha, domega)
 
@@ -70,9 +71,50 @@ real(kind=8) function GenericWave(sigshape ,ncyc, noffs, natt, nsus, ndcy, nhwhm
 
 end function GenericWave
 
+real(kind=8) function StepFunction(n,w,m)
+  real(kind=8) :: n, w, m
+  real(kind=8) :: x
 
+  x = n - (m*w)
+
+  StepFunction = 1./(exp((0.85*w-x)/(0.02*w))+1.)
+
+end function StepFunction
 !----------------------------------------------------------------------
+  real(kind=8) function RampWave(ncyc, noffs, natt, nsus, ndcy, nhwhm, omega, alpha, domega)
 
+    real(kind=8) :: ncyc, noffs, natt, nsus, ndcy, nhwhm, omega, alpha, domega
+    real(kind=8) :: amp, n, nend, awidth, dwidth, stepnr, sheight, m
+
+    ! copied from mathematica
+    n = ncyc - noffs
+    nend = natt + ndcy + nsus
+    awidth = natt/nhwhm
+    dwidth = ndcy/nhwhm
+    sheight = 1./nhwhm
+
+    if ( ( n .ge. 0 ) .and. ( n .le. ( nend - ( 0.5 * dwidth ) ) ) ) then
+       if ( n .le. natt ) then
+          stepnr = floor( n/awidth )
+          amp = (StepFunction(n,awidth,stepnr)+stepnr)*sheight
+       else
+          if ( n .ge. ( natt+nsus - dwidth ) ) then
+             m = n - ( natt+nsus );
+             stepnr = floor( m/dwidth )
+             amp = 1. - sheight*(StepFunction(m,dwidth,stepnr)+1.+stepnr)
+          else
+             amp = 1.
+          endif
+       endif
+    else
+       amp = 0.
+    endif
+   
+    RampWave = amp * cos( (omega+domega*(n-natt)*DT) * (n - natt) * DT + DEG*alpha)
+    
+    
+  end function RampWave
+    
 
   real(kind=8) function GaussianWave(ncyc, noffs, natt, nsus, ndcy, nhwhm, omega, alpha, domega)
 
